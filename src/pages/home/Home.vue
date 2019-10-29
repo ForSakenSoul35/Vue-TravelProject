e4ff62618ecebd94a7ab4f90980128def1005c86
forsakensoul35 <forsakensoul35@qq.com>
2019-10-29 09:26:57 +0800

    Vue project

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <div>hello home</div>
+    <home-header></home-header>
   </div>
 </template>
 <script>
+import HomeHeader from './components/Header'
 export default {
   // 导出组件的写法： 注意
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 <style scoped>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">内容</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+<script>
+export default {
+  // 导出组件的写法： 注意
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    justify-items: center
+    align-items: center
+    background-color #00bcd4
+    color: #fff
+    .header-left
+      width: .86rem
+    .header-right
+      width: 1.24rem
+      
+    .header-input
+      flex: 1
+</style>
